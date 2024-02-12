b1cda7c585ce79bd1028f91d2b207f49edfc44ee
anounymous <anounymous@none.com>
2024-02-12 20:03:51 +0330

    feat: (branch: feature/implement-database) => add relation method with Snapshot inside Machine model.

--- app/Models/Machine.php
+++ app/Models/Machine.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Machine extends Model
 {
     use HasFactory;
+
+    /**
+     * Relation method with Snapshot
+     *
+     * Each machine can have many snapshots while each snapshot
+     * is belongs to specific machine
+     *
+     * @return HasMany
+     */
+    public function snapshots():HasMany
+    {
+        return $this->hasMany(Snapshot::class);
+    }
 }
