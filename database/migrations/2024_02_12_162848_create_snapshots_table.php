f78ea495fb35fb78bae909ab6b118dedcf17a835
anounymous <anounymous@none.com>
2024-02-12 20:00:31 +0330

    feat: (branch: feature/implement-database) => complete snapshots table migration.

---
+++ database/migrations/2024_02_12_162848_create_snapshots_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('snapshots', function (Blueprint $table) {
+            $table->id();
+            $table->json('data');
+            $table->boolean('is_current')->default(0)->comment('Defines that is it snapshot current changes and was set on machine .');
+            $table->foreignId('machine_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('snapshots');
+    }
+};
