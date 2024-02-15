d13150af2f95948ce4c92be6f757b5656bdccd2c
anounymous <anounymous@none.com>
2024-02-15 16:43:18 +0330

    feat: (branch: feature/indexing-machines-unit-test) => add unit test for machine index url.

---
+++ tests/Feature/Http/Web/MachineController/IndexTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Http\Web\MachineController;
+
+use App\Models\Machine;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    /**
+     * Index machine route name
+     */
+    private const ROUTE_NAME = 'machine.index';
+
+    /**
+     * Asserts index view displays all of exists machines
+     *
+     * @return void
+     */
+    public function test_index_machine_view_display_all()
+    {
+        $machines = Machine::factory()->count(rand(1, 10))->create();
+
+        $res = $this->get(route(self::ROUTE_NAME));
+
+        $res->assertViewHasAll(['machines' => $machines->toArray()]);
+    }
+}
