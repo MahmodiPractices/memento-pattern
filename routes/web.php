f373dfaa09d8b47a85d972c8bb0a65bf43523314
anounymous <anounymous@none.com>
2024-02-18 20:28:35 +0330

    feat: (branch: feature/customize-aside) => cms ui template aside section customized.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
 
 Route::resource('machine', \App\Http\Controllers\MachineController::class);
